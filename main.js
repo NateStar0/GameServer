9b27877b486d0215c428dd86dbe1951293045189
NateStar0 <natcon0808@gmail.com>
2023-09-29 17:55:48 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+/*
+        --- Main.js ---
+
+        By Nahoo (29 / 9/ 2023)
+*/
+
+const net = require('net');
+const PORT = 15452;
+
+var clients = [];
+
+const server = net.createServer((socket) =>
+{
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;
+
+    clients.push(socket);
+
+    socket.on('data', (data) => 
+    {
+        socket.write(data);
+
+        console.log('data: ', data, data.toString())
+    });
+
+    socket.on('end', () =>
+    {
+        console.log('end, socket disconnected', socket.name);
+    });
+
+    socket.on('close', () =>
+    {
+        console.log('close, socket disconnected', socket.name);
+    });
+
+    socket.on('error', (error) => 
+    {
+        if(error.errno != -4077)
+        {
+            console.log('error, socket disconnected', error);
+        }
+    });
+});
+
+server.listen(PORT);
+
+console.log("Starting!");
